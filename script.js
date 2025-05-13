bbcd99226c01f0d428632606e0ec36013eafbf41
MarshalTOJO-dotcom <ghoshuddipan2002@gmail.com>
2025-05-13 17:02:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-document.querySelector(".fancy-button").addEventListener("click", function() {
-    this.style.backgroundColor = "#00ff99";
-    this.innerText = "Clicked!";
-    alert("You clicked the button!");
+// "Click Me" button interaction + GA event
+document.getElementById("clickMeBtn")?.addEventListener("click", function() {
+  this.style.backgroundColor = "#00ff99";
+  this.innerText = "Clicked!";
+  alert("You clicked the button!");
+
+  gtag('event', 'button_click', {
+    'event_category': 'engagement',
+    'event_label': 'Click Me Button',
+    'page_title': document.title,
+    'page_location': window.location.href
+  });
+});
+
+// Form Submit Button
+document.getElementById("submitBtn")?.addEventListener("click", function () {
+  gtag('event', 'form_submit', {
+    'event_category': 'contact',
+    'event_label': 'Contact Form Submit',
+    'page_title': document.title
+  });
+});
+
+// Video Play
+document.getElementById("myVideo")?.addEventListener("play", function () {
+  gtag('event', 'video_play', {
+    'event_category': 'media',
+    'event_label': 'Blog Video Play',
+    'page_title': document.title
+  });
+});
+
+// File Upload
+document.getElementById("fileUpload")?.addEventListener("change", function () {
+  gtag('event', 'file_upload', {
+    'event_category': 'interaction',
+    'event_label': 'User Uploaded File',
+    'page_title': document.title
+  });
+});
+
+// Scroll Depth (90%)
+window.addEventListener('scroll', function () {
+  const scrolled = (window.innerHeight + window.scrollY) / document.body.scrollHeight;
+  if (scrolled > 0.9 && !window.hasScrolledDeep) {
+    window.hasScrolledDeep = true;
+    gtag('event', 'scroll_depth', {
+      'event_category': 'engagement',
+      'event_label': 'Scrolled 90%',
+      'page_title': document.title
+    });
+  }
 });
